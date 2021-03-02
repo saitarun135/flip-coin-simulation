f7b5034c153b3a8ff9ff6b9dcaa2c51bfb77cf09
saitarun135 <saitarun135@gm.com>
2021-03-02 11:22:20 +0530

    win and how many times

--- flipcoinSimulator.sh
+++ flipcoinSimulator.sh
@@ -1,9 +1,28 @@
-#!/bin/bash -x
+#!/bin/bash 
 
-flipCoin=$((RANDOM%2))
-if [ $flipCoin -eq 1 ]
+echo  "Enter how many times to flip a coin :" 
+read num
+headCount=0;
+tailCount=0;
+ 		
+		for ((i=1; i<=$num; i++))
+			do
+			flipCoin=$((RANDOM%2))
+			if [ $flipCoin -eq 1 ]
+				then
+				((headCount++))
+				else 
+				((tailCount++))
+			fi
+			done
+echo "the heads count is $headCount and the tails count is $tailCount" 
+if [ $headCount -gt $tailCount ]
 then
-	echo HEAD
+	echo Heads is the winner
+elif [ $headCount -lt $tailCount ]
+then
+	echo Tails is the winner
 else 
-	echo TAILS
+	echo "it's a tie"
 fi
+
